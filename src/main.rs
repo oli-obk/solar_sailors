0f97e2062ecd09aab7db3b35a43859425a730db9
Oliver Scherer <github@oli-obk.de>
2021-07-24 18:11:23 +0200

    Add starscape

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use macroquad::prelude::*;
+use macroquad::rand::RandomRange;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let stars: Vec<_> = (0..100)
+        .map(|_| (f32::gen_range(0.0, 800.0), f32::gen_range(0.0, 600.0)))
+        .collect();
     loop {
-        clear_background(RED);
+        clear_background(BLACK);
+
+        for &(x, y) in &stars {
+            draw_star(Vec2::new(x, y), 5.0);
+        }
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
@@ -13,4 +21,12 @@ async fn main() {
 
         next_frame().await
     }
+}
+
+fn draw_star(pos: Vec2, size: f32) {
+    let y = f32::sin(std::f32::consts::PI / 3.0) * size;
+    let x = size / 2.0;
+    let left = Vec2::new(-x, y);
+    let right = Vec2::new(x, y);
+    draw_triangle(pos, pos + left, pos + right, WHITE)
 }

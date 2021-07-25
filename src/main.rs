a231846a0be0e8fa8609dd0982aa9bf98c07861e
Oliver Scherer <github@oli-obk.de>
2021-07-25 15:54:27 +0200

    Move ship into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use macroquad::prelude::*;
 use sail::Sail;
+use ship::SpaceShip;
 use stars::Stars;
 
 mod controlled;
 mod sail;
 mod stars;
+mod ship;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
@@ -29,37 +31,5 @@ async fn main() {
         draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 
         next_frame().await
-    }
-}
-
-struct SpaceShip {
-    pos: Vec2,
-    sail: Sail,
-    width: f32,
-    len: f32,
-}
-
-impl SpaceShip {
-    fn update(&mut self) {
-        self.sail.update();
-    }
-    fn draw(&self) {
-        self.sail.draw(self.pos);
-
-        // Spaceship
-        let mid = self.pos;
-        draw_triangle(
-            mid,
-            mid + Vec2::new(10.0, self.width / 2.0),
-            mid + Vec2::new(-10.0, self.width / 2.0),
-            BLUE,
-        );
-        draw_rectangle(
-            mid.x - self.width / 2.0,
-            mid.y + 10.0,
-            self.width,
-            self.len,
-            BLUE,
-        );
     }
 }
---
+++ src/ship.rs
@@ -0,0 +1,35 @@
+use macroquad::prelude::*;
+
+use crate::sail::Sail;
+
+pub(crate) struct SpaceShip {
+    pub(crate) pos: Vec2,
+    pub(crate) sail: Sail,
+    pub(crate) width: f32,
+    pub(crate) len: f32,
+}
+
+impl SpaceShip {
+    pub(crate) fn update(&mut self) {
+        self.sail.update();
+    }
+    pub(crate) fn draw(&self) {
+        self.sail.draw(self.pos);
+
+        // Spaceship
+        let mid = self.pos;
+        draw_triangle(
+            mid,
+            mid + Vec2::new(10.0, self.width / 2.0),
+            mid + Vec2::new(-10.0, self.width / 2.0),
+            BLUE,
+        );
+        draw_rectangle(
+            mid.x - self.width / 2.0,
+            mid.y + 10.0,
+            self.width,
+            self.len,
+            BLUE,
+        );
+    }
+}

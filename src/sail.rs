e041663ab3c8d077e5980be4c45de8343408c72f
Oliver Scherer <github@oli-obk.de>
2021-07-25 15:52:29 +0200

    Move sail into its own module

---
+++ src/sail.rs
@@ -0,0 +1,62 @@
+use macroquad::prelude::*;
+
+use crate::controlled::ButtonControlledRange;
+
+pub(crate) struct Sail {
+    left_rope: ButtonControlledRange,
+    right_rope: ButtonControlledRange,
+    sail_width: ButtonControlledRange,
+}
+
+impl Sail {
+    pub(crate) fn new(left_rope: f32, right_rope: f32, sail_width: f32) -> Self {
+        Self {
+            left_rope: ButtonControlledRange::new(left_rope, KeyCode::A),
+            right_rope: ButtonControlledRange::new(right_rope, KeyCode::D),
+            sail_width: ButtonControlledRange::new(sail_width, KeyCode::W),
+        }
+    }
+    pub(crate) fn update(&mut self) {
+        self.left_rope.update();
+        self.right_rope.update();
+        self.sail_width.update();
+    }
+    pub(crate) fn draw(&self, anchor: Vec2) {
+        let (left_x, left_y, right_x, right_y) = self.rope_positions(anchor);
+        // Sail
+        draw_line(left_x, left_y, right_x, right_y, 1.0, YELLOW);
+        // Ropes
+        draw_line(anchor.x, anchor.y, left_x, left_y, 1.0, GRAY);
+        draw_line(anchor.x, anchor.y, right_x, right_y, 1.0, GRAY);
+    }
+
+    /// Compute the position of the sail corners
+    fn rope_positions(&self, anchor: Vec2) -> (f32, f32, f32, f32) {
+        let angle = rope_angle(
+            self.left_rope.value,
+            self.right_rope.value,
+            self.sail_width.value,
+        );
+        let half_angle = angle / 2.0;
+        let x = half_angle.sin();
+        let y = half_angle.cos();
+        (
+            -x * self.left_rope.value + anchor.x,
+            -y * self.left_rope.value + anchor.y,
+            x * self.right_rope.value + anchor.x,
+            -y * self.right_rope.value + anchor.y,
+        )
+    }
+}
+
+/// Find the angle between "a" and "b" for a triangle with the given three side lengths.
+fn rope_angle(a: f32, b: f32, c: f32) -> f32 {
+    // http://mathcentral.uregina.ca/QQ/database/QQ.09.07/h/lucy1.html
+    // c^2 = a^2 + b^2 - 2ab cos(C)
+    // 2ab cos(C) = a^2 + b^2 - c^2
+    // cos(C) = (a^2 + b^2 - c^2)/(2ab)
+    let squares = a * a + b * b - c * c;
+    let divisor = 2.0 * a * b;
+    let cos_c = squares / divisor;
+    cos_c.acos()
+}

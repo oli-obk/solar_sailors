e4d504fd52b83dfbacf8db9fba91906f880a170a
Oliver Scherer <github@oli-obk.de>
2021-07-25 14:53:29 +0200

    draw sail relative to ship

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ async fn main() {
     let stars = Stars::new(100);
     let mid_x = screen_width() / 2.0;
     let mid_y = screen_height() / 2.0;
-    let sail = Sail::new(Vec2::new(mid_x, mid_y), 100.0, 100.0, 50.0);
+    let sail = Sail::new(100.0, 100.0, 50.0);
     let mut ship = SpaceShip {
         sail,
         len: 50.0,
         width: 20.0,
+        pos: Vec2::new(mid_x, mid_y),
     };
     loop {
         clear_background(BLACK);
@@ -43,7 +44,7 @@ impl Stars {
                 .collect(),
         }
     }
-    fn draw(&self){
+    fn draw(&self) {
         for &pos in &self.positions {
             draw_star(pos, 5.0);
         }
@@ -51,6 +52,7 @@ impl Stars {
 }
 
 struct SpaceShip {
+    pos: Vec2,
     sail: Sail,
     width: f32,
     len: f32,
@@ -61,10 +63,10 @@ impl SpaceShip {
         self.sail.update();
     }
     fn draw(&self) {
-        self.sail.draw();
+        self.sail.draw(self.pos);
 
         // Spaceship
-        let mid = self.sail.anchor;
+        let mid = self.pos;
         draw_triangle(
             mid,
             mid + Vec2::new(10.0, self.width / 2.0),
@@ -82,16 +84,14 @@ impl SpaceShip {
 }
 
 struct Sail {
-    anchor: Vec2,
     left_rope: ButtonControlledRange,
     right_rope: ButtonControlledRange,
     sail_width: ButtonControlledRange,
 }
 
 impl Sail {
-    fn new(anchor: Vec2, left_rope: f32, right_rope: f32, sail_width: f32) -> Self {
+    fn new(left_rope: f32, right_rope: f32, sail_width: f32) -> Self {
         Self {
-            anchor,
             left_rope: ButtonControlledRange::new(left_rope, KeyCode::A),
             right_rope: ButtonControlledRange::new(right_rope, KeyCode::D),
             sail_width: ButtonControlledRange::new(sail_width, KeyCode::W),
@@ -102,17 +102,17 @@ impl Sail {
         self.right_rope.update();
         self.sail_width.update();
     }
-    fn draw(&self) {
-        let (left_x, left_y, right_x, right_y) = self.rope_positions();
+    fn draw(&self, anchor: Vec2) {
+        let (left_x, left_y, right_x, right_y) = self.rope_positions(anchor);
         // Sail
         draw_line(left_x, left_y, right_x, right_y, 1.0, YELLOW);
         // Ropes
-        draw_line(self.anchor.x, self.anchor.y, left_x, left_y, 1.0, GRAY);
-        draw_line(self.anchor.x, self.anchor.y, right_x, right_y, 1.0, GRAY);
+        draw_line(anchor.x, anchor.y, left_x, left_y, 1.0, GRAY);
+        draw_line(anchor.x, anchor.y, right_x, right_y, 1.0, GRAY);
     }
 
     /// Compute the position of the sail corners
-    fn rope_positions(&self) -> (f32, f32, f32, f32) {
+    fn rope_positions(&self, anchor: Vec2) -> (f32, f32, f32, f32) {
         let angle = rope_angle(
             self.left_rope.value,
             self.right_rope.value,
@@ -122,10 +122,10 @@ impl Sail {
         let x = half_angle.sin();
         let y = half_angle.cos();
         (
-            -x * self.left_rope.value + self.anchor.x,
-            -y * self.left_rope.value + self.anchor.y,
-            x * self.right_rope.value + self.anchor.x,
-            -y * self.right_rope.value + self.anchor.y,
+            -x * self.left_rope.value + anchor.x,
+            -y * self.left_rope.value + anchor.y,
+            x * self.right_rope.value + anchor.x,
+            -y * self.right_rope.value + anchor.y,
         )
     }
 }

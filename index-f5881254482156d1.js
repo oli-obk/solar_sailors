6d3e88b6495c39b7afc6fb2f43f8cfdd6c66473b
oli-obk <oli-obk@users.noreply.github.com>
2021-08-04 14:59:56 +0000

    deploy: 71c717588999a9110e27d2e5a9e823e145229014

--- index-f5881254482156d1.js
+++ index-f5881254482156d1.js
@@ -35,7 +35,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-6badc62e19850644_bg.wasm', import.meta.url);
+        input = new URL('index-f5881254482156d1_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports['env'] = __wbg_star0;

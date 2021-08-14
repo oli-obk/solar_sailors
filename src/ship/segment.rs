dc8318ee4051704c5f671e6dbed8f8708ed8210e
Oliver Scherer <github@oli-obk.de>
2021-08-14 21:55:28 +0200

    Attachements are special

--- src/ship/segment.rs
+++ src/ship/segment.rs
@@ -1,4 +1,8 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{
+    cell::RefCell,
+    f32::consts::{FRAC_PI_3, PI},
+    rc::Rc,
+};
 
 use macroquad::prelude::*;
 
@@ -21,13 +25,27 @@ impl<T: Element> Element for Option<T> {
     }
 }
 
-impl<T: Element + ?Sized> Element for Rc<RefCell<T>> {
+impl<T: Attachement> Attachement for Option<T> {
+    fn update(&mut self) {
+        if let Some(this) = self {
+            this.update()
+        }
+    }
+
+    fn draw(&self, pos: Vec2, angle: f32) {
+        if let Some(this) = self {
+            this.draw(pos, angle)
+        }
+    }
+}
+
+impl<T: Attachement + ?Sized> Attachement for Rc<RefCell<T>> {
     fn update(&mut self) {
         self.borrow_mut().update()
     }
 
-    fn draw(&self, pos: Vec2) {
-        self.borrow().draw(pos)
+    fn draw(&self, pos: Vec2, angle: f32) {
+        self.borrow().draw(pos, angle)
     }
 }
 
@@ -42,7 +60,10 @@ impl<T: Element + ?Sized> Element for Box<T> {
 }
 
 pub trait Content: Element {}
-pub trait Attachement: Element {}
+pub trait Attachement {
+    fn update(&mut self);
+    fn draw(&self, pos: Vec2, angle: f32);
+}
 
 #[derive(Default)]
 pub struct Segment {
@@ -69,33 +90,32 @@ impl Element for Segment {
             BLUE,
         );
         self.content.draw(pos);
-        for attachement in &self.attachements {
-            attachement.draw(pos);
+        for (i, attachement) in self.attachements.iter().enumerate() {
+            attachement.draw(pos + ATTACHEMENT_OFFSETS[i], ATTACHEMENT_ANGLES[i]);
         }
     }
 }
 
+const ATTACHEMENT_ANGLES: [f32; 6] = [
+    0.0,
+    FRAC_PI_3,
+    FRAC_PI_3 * 2.0,
+    PI,
+    FRAC_PI_3 * 4.0,
+    FRAC_PI_3 * 5.0,
+];
+
 pub const SIZE: f32 = 40.0;
 
-fn hexagon_offset(position: Position) -> Vec2 {
+const ATTACHEMENT_OFFSETS: [Vec2; 6] = {
     let x = SIZE;
     let x2 = x / 2.0;
-    match position {
-        Position::Up => vec2(0.0, -x),
-        Position::RightUp => vec2(x2, -x2),
-        Position::RightDown => vec2(x2, x2),
-        Position::Down => vec2(0.0, x),
-        Position::LeftDown => vec2(-x2, x2),
-        Position::LeftUp => vec2(-x2, -x2),
-    }
-}
-
-/// Sides of a Hexagon, in clockwise order
-pub enum Position {
-    Up = 0,
-    RightUp = 1,
-    RightDown = 2,
-    Down = 3,
-    LeftDown = 4,
-    LeftUp = 5,
-}
+    [
+        const_vec2!([0.0, -x]),
+        const_vec2!([x2, -x2]),
+        const_vec2!([x2, x2]),
+        const_vec2!([0.0, x]),
+        const_vec2!([-x2, x2]),
+        const_vec2!([-x2, -x2]),
+    ]
+};

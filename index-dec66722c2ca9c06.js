e94c3a95a1fa638e7c7105a01b2c891b5946629e
oli-obk <oli-obk@users.noreply.github.com>
2021-08-19 23:00:08 +0000

    deploy: 75df896bab9cdf8357459e7959df6625c4e0eae4

--- index-dec66722c2ca9c06.js
+++ index-dec66722c2ca9c06.js
@@ -35,7 +35,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-3a5e02b540da2124_bg.wasm', import.meta.url);
+        input = new URL('index-dec66722c2ca9c06_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports['env'] = __wbg_star0;

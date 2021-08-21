0cbe5e5854f8d1292b268f8c4e50bf9adc1a0753
Oliver Scherer <github@oli-obk.de>
2021-08-21 19:10:03 +0200

    Crab!

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::{
     f32::consts::{FRAC_PI_2, FRAC_PI_3, FRAC_PI_4, PI},
 };
 
-use macroquad::prelude::*;
+use macroquad::prelude::{
+    animation::{AnimatedSprite, Animation, AnimationFrame},
+    *,
+};
 use sail::Sail;
 use ship::{Gauge, Segment, SpaceShip};
 use stars::Stars;
@@ -28,6 +31,61 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    let crab = Texture2D::from_file_with_format(
+        include_bytes!("../assets/redCrab.png"),
+        Some(ImageFormat::Png),
+    );
+    // Pixel art
+    crab.set_filter(FilterMode::Nearest);
+    let mut crabi = 0;
+    let mut crabx = 0.0;
+    let animations = &[
+        Animation {
+            name: "dance".into(),
+            row: 0,
+            frames: 7,
+            fps: 7,
+        },
+        Animation {
+            name: "walk left".into(),
+            row: 1,
+            frames: 4,
+            fps: 7,
+        },
+        Animation {
+            name: "walk right".into(),
+            row: 2,
+            frames: 4,
+            fps: 7,
+        },
+        Animation {
+            name: "idle".into(),
+            row: 3,
+            frames: 6,
+            fps: 7,
+        },
+        Animation {
+            name: "attack".into(),
+            row: 4,
+            frames: 7,
+            fps: 7,
+        },
+        Animation {
+            name: "sit".into(),
+            row: 5,
+            frames: 4,
+            fps: 7,
+        },
+        Animation {
+            name: "jump".into(),
+            row: 4,
+            frames: 3,
+            fps: 7,
+        },
+    ];
+    let mut anim = AnimatedSprite::new(16, 16, animations, true);
+    anim.set_animation(1);
+
     let screen = Rect::new(-400.0, -300.0, 800.0, 600.0);
     let stars = Stars::new(100, screen);
     let mut orbits = orbits::Orbits::new();
@@ -80,12 +138,23 @@ async fn main() {
     );
 
     let mut window = GameWindow::Ship;
-
+    let mut speed = 0;
     loop {
         // Logic
         ship.update();
         photons.update();
         orbits.update();
+        anim.update();
+        speed += 1;
+        if speed == 8 {
+            speed = 0;
+            crabi += 1;
+            crabi %= 4;
+            if crabi == 3 {
+                crabx += 3.0;
+            }
+        }
+        anim.set_frame(crabi);
 
         if is_key_pressed(KeyCode::M) {
             window = match window {
@@ -108,6 +177,21 @@ async fn main() {
                 stars.draw();
                 photons.draw();
                 ship.draw();
+                let AnimationFrame {
+                    source_rect,
+                    dest_size,
+                } = anim.frame();
+                draw_texture_ex(
+                    crab,
+                    crabx,
+                    264.0,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(dest_size * 3.0),
+                        source: Some(source_rect),
+                        ..Default::default()
+                    },
+                );
 
                 let pos = cam.screen_to_world(vec2(0.0, 0.0));
                 draw_text("M: view orbit", pos.x + 20.0, pos.y + 20.0, 30.0, DARKGRAY);

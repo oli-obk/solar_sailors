661035020967de5a8732c53f1eb0b73cbf5cd601
Oliver Scherer <github@oli-obk.de>
2021-08-25 14:50:09 +0200

    Install all the androids

--- android.sh
+++ android.sh
@@ -1,2 +1,2 @@
-rustup target add armv7-linux-androideabi aarch64-linux-android
+rustup target add armv7-linux-androideabi aarch64-linux-android i686-linux-android thumbv7neon-linux-androideabi x86_64-linux-android 
 cargo quad-apk build --release

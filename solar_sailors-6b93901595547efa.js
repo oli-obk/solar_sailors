848c78d3508fd2b3644a20847e8ee25167e12f7b
oli-obk <oli-obk@users.noreply.github.com>
2023-09-04 18:40:47 +0000

    deploy: 8d6ab54eae5255a45666c4a2cc31fddfe9a3dc0a

--- solar_sailors-6b93901595547efa.js
+++ solar_sailors-6b93901595547efa.js
@@ -73,7 +73,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('solar_sailors-892afe85ae403741_bg.wasm', import.meta.url);
+        input = new URL('solar_sailors-6b93901595547efa_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
